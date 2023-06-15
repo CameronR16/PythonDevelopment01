ffdfe19d33f547d69ba0fe491f89e6d73c5271de
Cameron Riley <cameron.riley321@gmail.com>
2023-06-15 19:46:31 +0100

    Variables and numbers file

---
+++ variables_numbers.py
@@ -0,0 +1,8 @@
+division_one = 37 / 2 #Standard division
+print(division_one)
+
+division_drop = 37 // 2 #Stores only the whole number and discards any numbers after the decimal place
+print(division_drop)
+
+division_rem = 37 % 2 #Stores remainder of divison ONLY
+print(division_rem)
