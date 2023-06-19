c29dcbbf3949032c3779bb39fc48070afd507b8d
Cameron Riley <cameron.riley321@gmail.com>
2023-06-19 21:05:18 +0100

    Adding lists file

---
+++ lists.py
@@ -0,0 +1,7 @@
+#Multiple values in a single variable
+#Different data types can be put into lists but its advised not to
+tvcharacters = ["Wednesday", "Enid", "Steve"]
+print(len(tvcharacters)) #Prints the length of the list
+tvcharacters.append("Eleven") #Adds a new element on the end
+print(tvcharacters)
+tvcharacters.remove("Steve") #Removes an elements from the list
